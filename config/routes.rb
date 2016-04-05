9036eb19211a9c05c87ef1bd96b723a0d08d519b
Matthew Riggott <Matthew.S.Riggott@gmail.com>
2016-04-05 15:25:05 -0400

    add precompile assets for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       resources :locations, only: [:index, :show]

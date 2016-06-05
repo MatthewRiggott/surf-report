f8f6cdfa1fc5b5a1be88d5779eaac16d6b2a563f
Matthew Riggott <Matthew.S.Riggott@gmail.com>
2016-06-05 02:02:45 -0400

    commit 1 line before change branch

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url = 'https://surf-report.herokuapp.com'
-
+    mail(to: @user.email, subject: "Welcome")
   end
 end
